b28d3aaf0eb03eb692ab51d82eb9ae3aac55ff47
Evan Leister <evanl@princeton.edu>
2012-11-20 16:23:57 -0500

    Added functions and first driver

---
+++ fd2_driver.py
@@ -0,0 +1,58 @@
+#!usr/bin/python
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+from time import clock, time
+import scipy
+
+import fd2_functions as fd2
+
+xw = 0.0
+xe = 70
+
+ys = 0.0
+yn = 70
+
+nx = 15
+ny = 15
+
+bc = fd2.BoundaryCondition("head", 10.0, "head", 10.0, "head", 10.0, "head", 10.0)
+
+dx = (xe - xw)/(nx-1)
+dy = (yn - ys)/(ny-1)
+
+Y,X = np.mgrid[ ys:yn:ny*1j, xw:xe:nx*1j]
+
+compressibility = 5.0 * pow(10,-10)
+thickness = 10.0;
+S = 1000.0 * 9.81 * compressibility * thickness
+
+tmax = 0.5
+nt = 5
+
+H1 = fd2.TimeSolve( xw , xe, ys, yn, nx, ny, dx, dy, bc, tmax, nt, S)
+
+HL = fd2.SpatialSolve(xw, xe, ys, yn, nx, ny, dx, dy, bc, t = 0, S = 0, dt = 1.0, r= np.zeros((1,1)), h0 = 11.0, K_B = 0.00005/5.0)
+
+fig1 = plt.figure()
+fig1.suptitle("Center-line in Domain")
+ax = fig1.add_subplot(111)
+for i in range(nt):
+	ax.plot( X[7,:], H1[7,:,i], label=("t = "+str(i * (tmax/(nt-1))) + " days") )
+ax.plot(X[7,:] , HL[7,:], label = "leaky case")
+ax.set_xlabel('x -direction [m]')
+ax.set_ylabel('average head [m]')
+ax.legend()
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+p1 = ax.plot(X[7,:] , HL[7,:])
+ax.set_xlabel('x-direction[m]')
+ax.set_ylabel('average hydraulic head [m]')
+fig.suptitle("Center-line in Domain")
+
+
+plt.show()
---
+++ fd2_functions.py
@@ -0,0 +1,228 @@
+import numpy as np
+
+
+
+class BoundaryCondition(object):
+    
+    def __init__(self, w_t = "head", w_v = 0,  \
+                       e_t = "head", e_v = 0, \
+                       s_t = "head", s_v = 0, \
+                       n_t = "head", n_v = 0 ):
+
+        # if a boundary condition value is entered as a function, make sure to change boundary condition routine in SpatialSolve
+        self.west_type = w_t
+        self.west_val = w_v
+ 
+        self.east_type = e_t
+        self.east_val = e_v
+
+        self.south_type = s_t
+        self.south_val = s_v
+
+        self.north_type = n_t
+        self.north_val = n_v
+
+#########################
+
+def Transmissivity(x, y):
+        
+    thickness = 10.0 
+    xcond = 0.001
+    ycond = 0.001
+    
+    T = []
+
+    T.append(thickness*xcond)
+    T.append(thickness*ycond)
+
+    return T
+
+##########################
+
+def Source(x,y,t): 
+    
+    #infiltration cases
+    #source = -0.0001 / (t + 1.0)
+    # source = -0.0001* np.sin(np.pi/4 * t )
+
+    # well case 
+    source = 0.0
+    if x > 32. and x < 38.:
+      if y > 32. and y < 38.:
+        source = 0.01 / (5. * 5.)
+
+    return source
+
+##########################
+
+# harmonic average convenience function
+def HarmAvg(T1, T2, dx1, dx2):
+    return (dx1 + dx2) / ( (dx1/T1) + (dx2/T2))
+
+##########################
+
+def SpatialSolve(xw, xe, ys, yn, nx, ny, dx, dy, bc, t = 0, S = 0, dt = 1.0, r= np.zeros((1,1)), h0 = 0.0, K_B =0.0): 
+    
+  N = (nx-2) * (ny -2)
+
+  H = np.zeros((ny,nx))
+  
+  if S == 0: 
+    A = np.zeros((N, N))
+  else:
+    A =  S / dt * np.eye(N) 
+
+  if r.all() == 0:    
+    r = np.zeros((N,1))
+
+  for i in xrange(N):
+    
+    # operations to determine the center node's global x and y coordinates. 
+    col = ( i % (nx-2) ) + 1        
+    row = i // (nx-2) + 1 
+    if col == 0 :
+      col = (nx-2)
+
+    x = xw + dx * col
+    y = ys + dy * row
+
+    r[i] += S / dt
+
+    r[i] = r[i] + dx * dy * Source(x,y,t) 
+
+
+    #leakage
+    if h0 != 0.0:
+      r[i] -= K_B * h0 * dx * dy
+      A[i,i] -= K_B * dx * dy
+    
+    TC = Transmissivity(x     , y     )
+    TW = Transmissivity(x - dx, y     )
+    TE = Transmissivity(x + dx, y     ) 
+    TS = Transmissivity(x     , y - dy)
+    TN = Transmissivity(x     , y + dy)
+
+    CW = 2.0 * dy / (dx + dx) * HarmAvg(TC[0], TW[0], dx, dx)
+    CE = 2.0 * dy / (dx + dx) * HarmAvg(TC[0], TE[0], dx, dx)
+    CS = 2.0 * dx / (dy + dy) * HarmAvg(TC[1], TS[1], dy, dy)
+    CN = 2.0 * dx / (dy + dy) * HarmAvg(TC[1], TN[1], dy, dy)
+
+    # am i on west face
+    if  col == 1 :
+      if bc.west_type == "flux":
+        r[i] = r[i] - bc.west_val
+      else:
+        r[i] = r[i] - CW * bc.west_val
+        A[i,i] = A[i,i] - CW
+    else:
+      A[i, i-1] = CW
+      A[i,i] = A[i,i] - CW
+
+    # east face
+    if col == (nx-2):
+      if bc.east_type == "flux":
+        r[i] = r[i] + bc.east_val
+      else:
+        r[i] = r[i] - CE * bc.east_val
+        A[i,i] = A[i,i] - CE
+    else:
+      A[i,i+1] = CE
+      A[i,i] = A[i,i] - CE
+
+    # south face
+    if i < (nx-2):
+      if bc.south_type == "flux":
+        r[i] = r[i] - bc.south_val
+      else:
+        r[i] = r[i] - CS * bc.south_val
+        A[i,i] = A[i,i] - CS
+    else:
+      A[i, i-(nx-2)] = CS
+      A[i,i] = A[i,i] - CS
+    
+    print ""
+    # north face
+    if i >= (N - (nx-2)):
+      if bc.north_type == "flux":
+        r[i] = r[i] + bc.north_val
+      else:
+        r[i] = r[i] - CN * bc.north_val
+        A[i,i] = A[i,i] - CN
+    else:
+      A[i,i +(nx-2)] = CN
+      A[i,i] = A[i,i] - CN
+
+  # end of  loop
+  
+  b = np.linalg.solve(A,r)
+
+  
+  # enter solved values into spatial head matrix
+
+  
+  for i in xrange(1,nx-1):
+      for j in xrange(1,ny-1):
+          H[j,i] = b[ (i-1) + (nx-2) * (j-1) ,0]
+
+  # note that boundary condition routine must be changed if the values are functions. 
+
+  for i in range(ny):
+      if bc.west_type == "head":
+          H[i,0] = bc.west_val            
+      else:
+          H[i,0] = H[i,1] + 1.0/dx * bc.west_val
+
+  for i in range(ny):
+      if bc.east_type == "head":
+          H[i,-1] = bc.east_val
+      else:
+          H[i,-1] = H[i,-2] - 1.0/dx * bc.east_val
+
+  for i in xrange(0,nx):
+      if bc.south_type == "head":
+          H[0,i] = bc.south_val
+      else:
+          H[0,i] = H[1,i] + 1.0/dy * bc.south_val
+
+  for i in xrange(0,nx):
+      if bc.north_type == "head":
+          H[-1,i] = bc.north_val
+      else:
+          H[-1,i] = H[-2,i] + 1/dy * bc.south_val
+
+  return H
+      
+###########################
+
+def TimeSolve( xw , xe, ys, yn, nx, ny, dx, dy, bc, tmax=1, nt=1, S=0, h0 = 0, K_B = 0):
+
+    N = (nx-2) * (ny-2)
+
+    r = np.zeros((N,1))
+
+    print tmax
+    print nt
+    dt = tmax / (nt-1)
+
+
+    # initial condition
+    
+    H = np.zeros((ny,nx,nt))
+    print np.shape(H)
+    H[:,:,0] = SpatialSolve(xw, xe, ys, yn, nx, ny, dx, dy, bc, h0, K_B)
+
+    for k in xrange(1,nt):
+        
+        count = 0
+        for i in xrange(1,nx-1):
+            for j in xrange(1,ny-1):
+                r[count,0] = S / dt * H[i,j,k-1]
+                count += 1
+        t = k * dt
+
+        H[:,:,k] = SpatialSolve(xw, xe, ys, yn, nx, ny, dx, dy, bc, t, S, dt, r, h0, K_B)
+
+    return H
+
+    
+    
