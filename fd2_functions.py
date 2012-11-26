corrected face velocity calculation, added some improved velocity criteria

fd2_functions.py
@@ -265,16 +265,15 @@ def TimeSolve( xw , xe, ys, yn, nx, ny, dx, dy, bc, tmax=1, nt=1, S=0, h0 = 0, K
 ##########################
 
 
-def CalcFaceVelocities( hC, hW, hE, hS, hN, k, phi):
+def CalcFaceVelocities( hC, hW, hE, hS, hN, dx, dy, k, phi):
   vel = []
-  vel.append(k * ( hW - hC) / phi)
-  vel.append( k * ( hC - hE) / phi)
-  vel.append( k * ( hS - hC) / phi)
-  vel.append( k * ( hC - hN) / phi)
+  vel.append(k * ( hW - hC) / (dx * phi))
+  vel.append( k * ( hC - hE) / (dx * phi))
+  vel.append( k * ( hS - hC) / (dy * phi))
+  vel.append( k * ( hC - hN) / (dy * phi))
   #returns w e s n
   return vel 
-def CalcPosition (x0, Ax, vx0, vxp, dt):
-  return  x0 + (vxp * np.exp( Ax * dt) - vx0) / Ax
+    
 def CheckExit(vx0, vx1, vxp):
   # returns False if no exit is possible in this direction. 
   # returns 0 if an exit across face 0 is possible. 
@@ -315,12 +314,18 @@ def CheckExit(vx0, vx1, vxp):
   else:
     print "Invalid velocity condition or input error"
     exit(1)
-    
+
+def CalcPosition (x0, Ax, vx0, vxp, dt):
+  if Ax > 1e-3:
+    return  x0 + (vxp * np.exp( Ax * dt) - vx0) / Ax
+  else:
+    return x0 + dt * vx0
+
 def CalcTravelTime(Ax, x0, x1, xp, vx0, vx1, vxp):
 # assumes exit face conditions have been checked
 # i.e. exit face is true and nonzero boundary velocities. 
   if (vx0 !=0 and vx1 !=0):
-    if (vx0 != vx1):
+    if (abs(vx1-vx0) > 1.e-6):
       if (vx0 > 0 and vx1 >0):
         return np.log(vx1/vxp) / Ax
       else:
@@ -335,9 +340,9 @@ def CalcTravelTime(Ax, x0, x1, xp, vx0, vx1, vxp):
 
 class StepReturn(object):
 
-  def __init__ (self, xnew, dTmin, exitdir, exitface):
+  def __init__ (self, xnew, dtMin, exitdir, exitface):
     self.xnew = xnew
-    self.dTmin = dTmin
+    self.dtMin = dtMin
     self.exitdir = exitdir
     self.exitface = exitface
 
@@ -351,6 +356,7 @@ def ParticleStep(xp, vp, v0, v1, x0, x1, dx):
     exit[i] = CheckExit(v0[i],v1[i],vp[i])
     A.append((v1[i] - v0[i])/dx[i])
     if exit[i] != False:
+      print vp[i]
       dt[i] = CalcTravelTime(A[i], x0[i], x1[i], xp[i], v0[i], v1[i], vp[i])
 
   # All velocities are into the current cell. 
@@ -362,11 +368,15 @@ def ParticleStep(xp, vp, v0, v1, x0, x1, dx):
   # some clever stuff here. exitdir is binary, 0 is x direction, 1 is y direction
   # exitface is 0 for lower, 1 for upper. 
   exitdir = dt.index(min(dt))
-  exitface = exit(exitdir)
+  exitface = exit[exitdir]
   
   xnew = []
   for i in range(2):
-    xnew.append(CalcPosition(xp[i], A[i], v0[i], vp[i], dtMin))
+    if (v0[i] !=0.0 and v1[i] !=0 ):
+      xnew.append(CalcPosition(xp[i], A[i], v0[i], vp[i], dtMin))
+    else:
+      xnew.append(xp[i])
+
   
   step = StepReturn( xnew, dtMin, exitdir, exitface)
   return step

fd2_particle.py
@@ -9,7 +9,10 @@ import sys
 
 import fd2_functions as fd2
 
-xw = 0.0; xe = 70; ys = 0.0; yn = 70;
+k = 50
+phi = 0.25
+
+xw = 0.0; xe = 5000; ys = 0.0; yn = 5000;
 
 if len(sys.argv) == 1 :
   print "please specify nx"
@@ -19,14 +22,6 @@ ns = int(sys.argv[1])
 
 nx = ns
 ny = ns
-print nx , ny 
-
-compressibility = 5.0 * pow(10,-10)
-thickness = 10.0;
-S = 1000.0 * 9.81 * compressibility * thickness
-
-tmax = 0.5
-nt = 5
                           #west,        east        south        north
 bc = fd2.BoundaryCondition("head",25.0,"head", 0.0,"flux", 0.0,"flux", 0.0)
 
@@ -40,7 +35,6 @@ Y,X = np.mgrid[ ys:yn:ny*1j, xw:xe:nx*1j]
 H = fd2.SpatialSolve(xw, xe, ys, yn, nx, ny, dx, dy, bc, t = 0, S = 0, dt = 1.0, r= np.zeros((1,1)), h0 = 0.0 , K_B = 0. )
 
 tFinal = time()
-
 
 print "Nx = " + str(nx)
 print "Solve time=  " + str( tFinal - tInit)
@@ -59,17 +53,19 @@ if plottype !=0:
     ax = Axes3D(fig)
     surf = ax.plot_surface(X,Y,H, rstride=1, cstride =1, linewidth =0, cmap = cm.jet, antialiased=False)
     CB = plt.colorbar(surf, shrink = 0.8, extend = 'both')
+    ax.set_xlabel('x-direction')
+    ax.set_ylabel('y-direction')
   #plt.show()
 plt.savefig('head.png')
 # particle tracking stuff goes here: 
 
-tpart = 1
-t = 0
+tpart = 500. 
+t = 0.
 
 xpart = []
 ypart = []
-xpart.append( 5.)
-ypart.append( 5.)
+xpart.append( 100.)
+ypart.append( 100.)
 
 
 i=0
@@ -97,7 +93,7 @@ dxi.append( dx)
 
 while t < tpart and outOfDomain == False :
   
-  vel = fd2.CalcFaceVelocities( H[j,i], H[j,i-1], H[j,i+1], H[j-1,i], H[j+1,i], 0.001, 0.25)
+  vel = fd2.CalcFaceVelocities( H[j,i], H[j,i-1], H[j,i+1], H[j-1,i], H[j+1,i], dx, dy, k, phi)
   #creates vectors to pass into step function
   v0,v1,x0,x1=[0]*2, [0]*2, [0]*2, [0]*2  
   
@@ -114,13 +110,15 @@ while t < tpart and outOfDomain == False :
 
   step = fd2.ParticleStep(xp, vp, v0, v1, x0, x1, dxi)
 
-  if step.dTmin != False:
+  if step.dtMin != False:
     # increment necessary values to get to next timestep. 
     t += step.dtMin    
     xpart.append(step.xnew[0])
     ypart.append(step.xnew[1])
     xp[0] = step.xnew[0]
     xp[1] = step.xnew[1]
+    #vp[0] = step.vout[0]
+    #vp[1] = step.vout[1]
     if step.exitdir == 0:
       if step.exitface == 1 :
         i +=1
@@ -134,4 +132,6 @@ while t < tpart and outOfDomain == False :
   else:
     outOfDomain = True
 
-  
+print xpart
+print ypart
+print t